462b01ae749288a7457b58fa33677d736ac5728f
theo <theoemail@internet.ru>
2024-12-19 15:26:32 +0300

    gui redos

--- client/abonentManager.cpp
+++ client/abonentManager.cpp
@@ -2,6 +2,11 @@
 #include <QApplication>
 #include <QDebug>
 #include <QFrame>
+#include <QVBoxLayout>
+#include <QComboBox>
+#include <QDialogButtonBox>
+#include <QMessageBox>
+#include <QtDebug>
 
 AbonentManager::AbonentManager(QWidget* parent) : QWidget(parent)
 {
@@ -14,8 +19,22 @@ AbonentManager::AbonentManager(QWidget* parent) : QWidget(parent)
     QWidget* containerCon = new QWidget();
     scrollAreaCon->setWidget(containerCon);
     layoutCon = new QVBoxLayout(containerCon);
-    headerLabelCon = new QLabel("Список соединений (0)", containerCon);
-    layoutCon->addWidget(headerLabelCon);
+    // Add buttons for increasing and decreasing connections
+    increaseConnectionsButton = new QPushButton("Макс. Кол-во соединений +1", this);
+    decreaseConnectionsButton = new QPushButton("Макс. Кол-во соединений -1", this);
+    connect(increaseConnectionsButton, &QPushButton::clicked, this, &AbonentManager::increaseMaxConnections);
+    connect(decreaseConnectionsButton, &QPushButton::clicked, this, &AbonentManager::decreaseMaxConnections);
+    layoutCon->addWidget(increaseConnectionsButton);
+    layoutCon->addWidget(decreaseConnectionsButton);
+
+    // Create the "Создать соединение" button
+    createConnectionButton = new QPushButton("Создать соединение", this);
+    connect(createConnectionButton, &QPushButton::clicked, this, &AbonentManager::showCreateConnectionDialog);
+    layoutCon->addWidget(createConnectionButton); // Add the button to the left block
+
+    createButton = new QPushButton("Создать абонента", this);
+    layoutCon->addWidget(createButton);
+    connect(createButton, &QPushButton::clicked, this, &AbonentManager::showCreateDialog);
     horizontalLayout->addWidget(scrollAreaCon);
 
     // Center Block - ATS Information with button
@@ -25,11 +44,17 @@ AbonentManager::AbonentManager(QWidget* parent) : QWidget(parent)
     atsInfoFrame->setContentsMargins(5, 5, 5, 5);
     QVBoxLayout* atsInfoLayout = new QVBoxLayout(atsInfoFrame);
     atsInfoLayout->addWidget(new QLabel("Информация об АТС", atsInfoFrame));
+
+    // Add new label for maximum connections
+    maxConnectionsLabel = new QLabel("Максимальное количество соединений: 0", atsInfoFrame);
+    atsInfoLayout->addWidget(maxConnectionsLabel);
+
+    headerLabelCon = new QLabel("Список соединений (0)", atsInfoFrame);
+    atsInfoLayout->addWidget(headerLabelCon);
+
     atsInfoLayout->addWidget(new QLabel("Версия АТС: 1.0", atsInfoFrame));
     atsInfoLayout->addWidget(new QLabel("Статус АТС: Работает", atsInfoFrame));
-    createButton = new QPushButton("Создать абонента", this);
-    atsInfoLayout->addWidget(createButton);
-    connect(createButton, &QPushButton::clicked, this, &AbonentManager::showCreateDialog);
+
     horizontalLayout->addWidget(atsInfoFrame);
 
     // Right Block - List of Abonents
@@ -46,6 +71,107 @@ AbonentManager::AbonentManager(QWidget* parent) : QWidget(parent)
 
     // Draw table of abonents
     drawAbonentsTable();
+
+    connect(&ats, &ATS::messageReceived, this, [this](QString from, QString to, QString message)
+    {
+
+        // Find the corresponding chat window and append the message
+
+        // You may need to maintain a list of chat windows to find the correct one
+
+        for (auto chatWindow : chatWindows)
+        {
+            // Assuming you maintain a list of chat windows
+
+            if ((chatWindow->fromPhone == from && chatWindow->toPhone == to) ||
+
+                (chatWindow->fromPhone == to && chatWindow->toPhone == from))
+            {
+
+                chatWindow->appendMessage(from + ": " + message);
+
+                break;
+            }
+        }
+    });
+}
+
+// Implement the removeSelectedAbonent slot
+void AbonentManager::removeSelectedAbonent()
+{
+
+}
+
+
+// Implement the showCreateConnectionDialog slot
+void AbonentManager::showCreateConnectionDialog()
+{
+    QDialog* createConnectionDialog = new QDialog(this);
+    createConnectionDialog->setWindowTitle("Создать соединение");
+    QVBoxLayout* dialogLayout = new QVBoxLayout(createConnectionDialog);
+
+
+    // Create combo boxes for selecting abonents
+    QComboBox* abonent1ComboBox = new QComboBox(createConnectionDialog);
+    QComboBox* abonent2ComboBox = new QComboBox(createConnectionDialog);
+
+
+    // Populate the combo boxes with abonents
+    QList<Abonent*> abonentsList = ats.getAllAbonents(); // Use the new method
+    for (Abonent* abonent : abonentsList)
+    {
+        abonent1ComboBox->addItem(abonent->getName(), abonent->getPhone());
+        abonent2ComboBox->addItem(abonent->getName(), abonent->getPhone());
+    }
+
+
+    dialogLayout->addWidget(new QLabel("Выберите первого абонента:", this));
+    dialogLayout->addWidget(abonent1ComboBox);
+    dialogLayout->addWidget(new QLabel("Выберите второго абонента:", this));
+    dialogLayout->addWidget(abonent2ComboBox);
+
+
+    // Create buttons for OK and Cancel
+    QDialogButtonBox* buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, createConnectionDialog);
+
+    connect(buttonBox, &QDialogButtonBox::accepted, createConnectionDialog, &QDialog::accept);
+    connect(buttonBox, &QDialogButtonBox::rejected, createConnectionDialog, &QDialog::reject);
+    dialogLayout->addWidget(buttonBox);
+
+    if (currentConnections < maxConnections && createConnectionDialog->exec() == QDialog::Accepted)
+    {
+        currentConnections++;
+
+        QString abonent1Phone = abonent1ComboBox->currentData().toString();
+
+        QString abonent2Phone = abonent2ComboBox->currentData().toString();
+
+        // Logic to create the connection
+        initiateCall(abonent1Phone, abonent2Phone);
+
+        ChatWindow* chatWindowA = new ChatWindow(abonent1Phone, abonent2Phone, this);
+        ChatWindow* chatWindowB = new ChatWindow(abonent2Phone, abonent1Phone, this);
+
+        // Connect signals to send messages through ATS
+        connect(chatWindowA, &ChatWindow::messageSent, this, &AbonentManager::sendMessage);
+        connect(chatWindowB, &ChatWindow::messageSent, this, &AbonentManager::sendMessage);
+
+        // Add chat windows to the list
+        chatWindows.append(chatWindowA);
+        chatWindows.append(chatWindowB);
+
+        // Show both chat windows
+        chatWindowA->show();
+        chatWindowB->show();
+    }
+    // Clean up
+    delete createConnectionDialog;
+}
+
+void AbonentManager::sendMessage(const QString& fromPhone, const QString& toPhone, const QString& message) {
+
+    ats.sendMessage(fromPhone, toPhone, message);
+
 }
 
 void AbonentManager::showCreateDialog()
@@ -81,41 +207,43 @@ void AbonentManager::addAbonent(const QString& name, const QString& phone)
 {
     QWidget* abonentWidget = new QWidget(this);
     QHBoxLayout* abonentLayout = new QHBoxLayout(abonentWidget);
-
     QLabel* nameLabel = new QLabel(name, abonentWidget);
     QLabel* phoneLabel = new QLabel(phone, abonentWidget);
 
-    QPushButton* deleteButton = new QPushButton("Удалить", abonentWidget);
-    QPushButton* callButton = new QPushButton("Позвонить", abonentWidget);
-    QPushButton* messageButton = new QPushButton("Отправить сообщение", abonentWidget);
+    // Add a status label
+    QLabel* statusLabel = new QLabel("Готов", abonentWidget); // You can change the status as needed
+//    QPushButton* deleteButton = new QPushButton("Удалить", abonentWidget);
 
-    connect(deleteButton, &QPushButton::clicked, this, [this, abonentWidget]()
-    {
-        removeAbonent(abonentWidget);
-    });
+//    connect(deleteButton, &QPushButton::clicked, this, [this, abonentWidget]()
+//    {
+//        removeAbonent(abonentWidget);
+//    });
 
-    connect(callButton, &QPushButton::clicked, this, [this, phone]()
-    {
-        initiateCall("YourPhoneNumber", phone); // Replace "YourPhoneNumber" with the actual caller's phone number
-    });
+    // Only add the delete button and status label
+    abonentLayout->addWidget(nameLabel);
+    abonentLayout->addWidget(phoneLabel);
+    abonentLayout->addWidget(statusLabel); // Add the status label
+//    abonentLayout->addWidget(deleteButton); // Add the delete button
 
-   connect(messageButton, &QPushButton::clicked, this, [this, phone]()
-   {
-       sendMessage("YourPhoneNumber", phone, "Hello!"); // Replace "YourPhoneNumber" and message as needed
-   });
+    layout->addWidget(abonentWidget);
 
-   abonentLayout->addWidget(nameLabel);
-   abonentLayout->addWidget(phoneLabel);
-   abonentLayout->addWidget(callButton);
-   abonentLayout->addWidget(messageButton);
-   abonentLayout->addWidget(deleteButton);
 
-   layout->addWidget(abonentWidget);
+    abonentsWidget.push_back(abonentWidget);
 
-   abonentsWidget.push_back(abonentWidget);
+    ats.addAbonent(name, phone, 2228); // Add abonent to ATS // TODO: 2228 - ZAGLUSHKA - SET UP WITH NETW
+    updateHeader();
 
-   ats.addAbonent(name, phone, 2228); // Add abonent to ATS // TODO: 2228 - ZAGLUSHKA - SET UP WITH NETW
-   updateHeader();
+//    connect(callButton, &QPushButton::clicked, this, [this, phone]()
+//    {
+//        initiateCall("YourPhoneNumber", phone); // Replace "YourPhoneNumber" with the actual caller's phone number
+//    });
+
+//   connect(messageButton, &QPushButton::clicked, this, [this, phone]()
+//   {
+//       sendMessage("YourPhoneNumber", phone, "Hello!"); // Replace "YourPhoneNumber" and message as needed
+//   });
+
+
 }
 
 void AbonentManager::removeAbonent(QWidget* abonent)
@@ -147,10 +275,23 @@ void AbonentManager::initiateCall(const QString& callerPhone, const QString& tar
    ats.initiateCall(callerPhone, targetPhone);
 }
 
-void AbonentManager::sendMessage(const QString& fromPhone, const QString& toPhone, const QString& message)
+
+void AbonentManager::increaseMaxConnections()
 {
-   ats.sendMessage(fromPhone, toPhone, message);
+    maxConnections++;
+    maxConnectionsLabel->setText(QString("Максимальное количество соединений: %1").arg(maxConnections));
 }
+
+
+void AbonentManager::decreaseMaxConnections()
+{
+    if (maxConnections > 0)
+    {
+        maxConnections--;
+        maxConnectionsLabel->setText(QString("Максимальное количество соединений: %1").arg(maxConnections));
+    }
+}
+
 
 AbonentManager::~AbonentManager()
 {
--- client/abonentManager.h
+++ client/abonentManager.h
@@ -14,9 +14,12 @@
 #include <QDialog>
 #include <QMap>
 #include <QVector>
+#include <QComboBox> // Include QComboBox for selecting abonents
+
 #include "ats.h" // Include ATS header
 #include "abonent.h" // Include Abonent header
 
+#include "chatWindow.h"
 
 class AbonentManager : public QWidget
 {
@@ -34,11 +37,19 @@ private slots:
     void drawAbonentsTable();
     void initiateCall(const QString& callerPhone, const QString& targetPhone);
     void sendMessage(const QString& fromPhone, const QString& toPhone, const QString& message);
+    void increaseMaxConnections();
+    void decreaseMaxConnections();
+    void showCreateConnectionDialog(); // New slot for creating a connection
+    void removeSelectedAbonent(); // New slot for removing selected abonent
 
 private:
     QLabel* headerLabel;
     QLabel* headerLabelCon;
+    QLabel* maxConnectionsLabel; // New label for max connections
     QPushButton* createButton;
+    QPushButton* increaseConnectionsButton;
+    QPushButton* decreaseConnectionsButton;
+    QPushButton* createConnectionButton; // New button for creating a connection
     QVBoxLayout* layout;
     QVBoxLayout* layoutCon;
     QVector<QWidget*> abonentsWidget;
@@ -49,9 +60,20 @@ private:
     QLineEdit* nameInput;
     QLineEdit* phoneInput;
 
+    // Create Connection Dialog components
+    QDialog* createConnectionDialog; // New dialog for creating a connection
+    QComboBox* abonent1ComboBox; // ComboBox for selecting the first abonent
+    QComboBox* abonent2ComboBox; // ComboBox for selecting the second abonent
+
+
+    // ComboBox for selecting an abonent to delete
+    QComboBox* abonentToDeleteComboBox; // New combo box for selecting an abonent to delete
 
     // ATS instance
     ATS ats; // Instance of ATS to manage abonents
+    int maxConnections = 0; // TODO: redo with ats's internal amount of maxConnections
+    int currentConnections = 0;
+    QList<ChatWindow*> chatWindows;
 
 };
 
